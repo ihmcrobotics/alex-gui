1a5ce2969790157f27655c79dd0c45097821eac6
Reese Peterson <rpeterson@ihmc.org>
2026-06-09 11:55:44 -0500

    Added urder merger

---
+++ urdf_helper.py
@@ -0,0 +1,87 @@
+from typing import List
+
+from lxml import etree
+
+ALEX_NUBFOREARMS_PARTS = ["head", "leftUpperArm", "leftFixedForearm", "rightUpperArm", "rightFixedForearm"]
+ALEX_CYCLOIDFOREARMS_PARTS = ["head", "leftUpperArm", "leftForearm", "rightUpperArm", "rightForearm"]
+
+ALEX_FULLBODY_PARTS = ["head", "leftUpperArm", "leftForearm", "leftAbilityHandAdapter", "ability_hand_left_large", "rightUpperArm", "rightForearm", "rightAbilityHandAdapter", "ability_hand_right_large"]
+ALEX_NOARMS_PARTS = ["head"]
+
+ALEX_URDF_PATH = "ihmc-alex-sdk/alex-models/alex_description/urdf/"
+HANDS_PATH = "ihmc-alex-sdk/alex-ros2/ihmc_hands_ros2/"
+
+def merge_urdfs(robot_version: str, wanted_parts: List[str], fixed_joints: List[str] = [""],
+                allowed_collisions: List[str] | None = None, output_name: str = "temp"):
+    prefix = "alex_"
+
+    excluded_name = "IMU"
+
+    main_directory = ALEX_URDF_PATH + robot_version + '/'
+    hand_mesh_directory = HANDS_PATH + "meshes/"
+    robot_prefix = prefix + "v1"
+    if robot_version == "002":
+        robot_prefix = prefix + "v2"
+    print(robot_prefix)
+    base_urdf_file = main_directory + robot_prefix + ".lowerBody.urdf"
+    base_urdf = etree.parse(base_urdf_file)
+    base_robot = base_urdf.getroot()
+
+    for joint in base_robot.findall("joint"):
+        if (excluded_name in joint.get("name", "")):
+            base_robot.remove(joint)
+    for link in base_robot.findall("link"):
+        if (excluded_name in link.get("name", "")):
+            base_robot.remove(link)
+        for visual in link.findall("visual"):
+            mesh_location = visual.find("geometry/mesh").get("filename")
+            new_location = mesh_location.replace("package://",
+                                                 "/home/rpeterson/repos/python-repo/ihmc-alex-sdk/alex-models/")
+            visual.find("geometry/mesh").set("filename", new_location)
+    for gazebo in base_robot.findall("gazebo"):
+        if (excluded_name in gazebo.get("reference", "")):
+            base_robot.remove(gazebo)
+
+    for wanted_part in wanted_parts:
+        if ("ability_hand" in wanted_part):
+            directory = HANDS_PATH + "urdf/abilityHand/"
+            filename = wanted_part + ".urdf"
+        else:
+            directory = main_directory
+            filename = robot_prefix + "." + wanted_part + ".urdf"
+        curr_urdf = etree.parse(directory + filename).getroot()
+
+        for joint in curr_urdf.findall("joint"):
+            if (excluded_name not in joint.get("name", "")):
+                if (joint.get("name", "") in fixed_joints or "hand" in wanted_part):
+                    joint.set("type", "fixed")
+                base_robot.append(joint)
+
+        for link in curr_urdf.findall("link"):
+            if (excluded_name not in link.get("name", "")):
+                if ("hand" in wanted_part):
+                    for visual in link.findall("visual"):
+                        mesh_location = visual.find("geometry/mesh").get("filename")
+                        new_location = mesh_location.replace("package://", hand_mesh_directory)
+                        visual.find("geometry/mesh").set("filename", new_location)
+                else:
+                    for visual in link.findall("visual"):
+                        mesh_location = visual.find("geometry/mesh").get("filename")
+                        new_location = mesh_location.replace("package://", "/home/rpeterson/repos/python-repo/ihmc-alex-sdk/alex-models/")
+                        visual.find("geometry/mesh").set("filename", new_location)
+                base_robot.append(link)
+        for gazebo in curr_urdf.findall("gazebo"):
+            if (excluded_name not in gazebo.get("reference", "")):
+                base_robot.append(gazebo)
+
+    if allowed_collisions is not None:
+        for link in base_robot.findall(".//link"):
+            for collision in link.findall("collision"):
+                print(collision.get("name"))
+                if collision.get("name") not in allowed_collisions:
+                    link.remove(collision)
+
+    save_filepath = main_directory + output_name + ".urdf"
+    base_urdf.write(save_filepath, pretty_print=True, xml_declaration=True, encoding="UTF-8")
+
+    return save_filepath
